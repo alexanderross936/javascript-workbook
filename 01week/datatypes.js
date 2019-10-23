a8a88104cb5c31e2e867851fc804dc4523e9f792
alexanderross936 <35351365+alexanderross936@users.noreply.github.com>
2019-10-22 20:00:36 -0500

    Update datatypes.js

---
+++ 01week/datatypes.js
@@ -0,0 +1,56 @@
+//make a variable called name and assign it's value to be your name as a string
+const name = 'Alex';
+
+//make a variable called userAge and assign it's value to be your age as a number
+const userAge = 29;
+
+//create a function called dividedBy, it should take in any two numbers and return the result of the first number divided by the second
+
+const dividedBy = (num1, num2) => {
+  return num1/num2;
+}
+
+console.log('hello');
+dividedBy(8, 2);
+const date = Date();
+console.log(date);
+const string = toString(7);
+const number = parseInt(string)
+console.log(string);
+2 + 2;
+num1 = 1;
+num2 = 2;
+num3 = 3;
+num4 = 3;
+const myFunc = (condition1, condition2) => {
+if (condition1 && condition2) {
+  console.log("Both conditions are true.")
+} else if (condition1 || condition2){
+  console.log("One condition is true")
+} else {
+  console.log("Neither condition is true.")
+}
+}
+
+myFunc(false, false);
+const yes = true;
+const foo = null;
+const bar = undefined;
+const numero = 7;
+const not = NaN;
+const str = "Hello.";
+
+// your variable
+var myBool = false;
+
+// function declaration
+function isTypeOf(data) {
+  return console.log(typeof data);
+}
+
+// function invocation
+isTypeOf(str);
+
+// replace "myBool" with the other names of your variable each time you run it
+
+// OR copy/paste "isTypeOf()" once for each variable you have and place each variable name inside the "()". Make sure your variables are above your function invocation, else they will be "undefined"!
